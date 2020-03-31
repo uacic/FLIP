350e9cfa55f4a5b87e46c77dea262bd879317933
John Xu <zhxu73@email.arizona.edu>
2020-03-31 08:33:38 -0700

    bin2png able to specify output dir

--- bin_conversion.py
+++ bin_conversion.py
@@ -11,35 +11,42 @@ import os
 import PIL
 from PIL import Image
 
-def bin_to_png(filepath):
+def bin_to_png(input_path, output_dir):
     """converts a single .bin image to png"""
     # written by David Moller
     try:
-        raw_data = open(filepath, 'rb').read()
+        raw_data = open(input_path, 'rb').read()
         img_size = (1936, 1216)
         img = PIL.Image.frombytes('L', img_size, raw_data, 'raw')
-        savename = filepath[:-4] + '.png'
-        img.save(savename)
+        savename = os.path.basename(input_path)[:-4] + '.png'
+        output_path = os.path.join(output_dir, savename)
+        img.save(output_path)
         print(savename, "converted")
     except Exception as e:
-        print(f"{filepath}: {e}")
+        print(f"{input_path}: {e}")
 
-def convert_dir(directory):
+def convert_dir(input_dir, output_dir):
     """converts all the .bin images in a directory to .png"""
 
-    for f in os.listdir(directory):
+    for f in os.listdir(input_dir):
         if f.endswith(".bin"):
-            bin_to_png(os.path.join(directory, f))
+            bin_to_png(os.path.join(input_dir, f), output_dir)
 
-def convert_dirs(directory, processes=-1):
+def convert_dirs(input_base_dir, output_base_dir, processes=-1):
     """calls convert dir in a new process on each
     of the subdirectories in a given directory"""
 
     # finding all directories in the given dir
     dirs_to_process = []
-    for root, dirs, files, in os.walk(directory):
-        if files:
-            dirs_to_process.append(root)
+    output_dirs = []
+    with os.scandir(input_base_dir) as it:
+        for entry in it:
+            if entry.is_dir():
+                dirs_to_process.append(entry.path)
+                # create sub dir of the same name as the input
+                output_dir = os.path.join(output_base_dir, entry.name)
+                os.mkdir(output_dir)
+                output_dirs.append(output_dir)
 
     # using the max number of processes if none are specified
     if processes < 1:
@@ -47,7 +54,7 @@ def convert_dirs(directory, processes=-1):
 
     # opening a new process for each directory found
     with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
-        p.map(convert_dir, dirs_to_process)
+        p.map(convert_dir, dirs_to_process, output_dirs)
 
     print("Finished converting to pngs in", directory)
 
